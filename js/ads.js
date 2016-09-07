a380c18474cd18d99495d7d389b07bac1f76d849
Benjamin Franzi <benjamin.franzi@outlook.com>
2016-09-08 00:24:41 +1000

    ad blocker notice

---
+++ js/ads.js
@@ -0,0 +1,4 @@
+var e=document.createElement('div');
+e.id='XVNxdfQBsEeI';
+e.style.display='none';
+document.body.appendChild(e);
---
+++ js/checkad.js
@@ -0,0 +1,7 @@
+if(document.getElementById('XVNxdfQBsEeI')){
+  console.log('Blocking Ads: No');
+
+} else {
+  console.log('Blocking Ads: Yes');
+  document.getElementById("ad_warn").innerHTML = "If you are having trouble viewing the website your adblocker is enabled";
+}
