3f4e3e32b62003dee93c5b772a6776668f391b96
Ben Franzi <benfranzi@outlook.com>
2018-08-02 18:31:35 +1000

    restoring site

--- js/checkad.js
+++
@@ -1,7 +0,0 @@
-if(document.getElementById('XVNxdfQBsEeI')){
-  console.log('Blocking Ads: No');
-
-} else {
-  console.log('Blocking Ads: Yes');
-  document.getElementById("ad_warn").innerHTML = "If you are having trouble viewing the website your adblocker is enabled";
-}
